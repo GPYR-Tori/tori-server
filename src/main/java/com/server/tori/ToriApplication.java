f6baade5ed04ffb280ef5b7e0fd26deb256841f8
JUNE0823 <jhjune0823@naver.com>
2023-10-24 23:57:35 +0900

    project setting

---
+++ src/main/java/com/server/tori/ToriApplication.java
@@ -0,0 +1,13 @@
+package com.server.tori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToriApplication.class, args);
+	}
+
+}
