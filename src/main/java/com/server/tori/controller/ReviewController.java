d6254ab5b07876325f8031989f91a131e2310d06
수현 <su551732@gmail.com>
2023-12-19 05:54:42 +0900

    :truck: rename: 변수명 수정

--- src/main/java/com/server/tori/controller/ReviewController.java
+++ src/main/java/com/server/tori/controller/ReviewController.java
@@ -3,6 +3,7 @@ package com.server.tori.controller;
 import com.server.tori.dto.Review.*;
 import com.server.tori.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,26 +17,26 @@ public class ReviewController {
 
     // 리뷰 생성
     @PostMapping
-    public ReviewWriteResponseDto postReview(@PathVariable("landmarkId") Long landmarkId, @RequestBody ReviewWriteRequestDto reviewWriteRequestDto){
-        return reviewService.postReview(landmarkId, reviewWriteRequestDto);
+    public ResponseEntity<ReviewCreateResponseDto> createReview(@PathVariable("landmarkId") Long landmarkId, @RequestBody ReviewCreateRequestDto reviewCreateRequestDto){
+        return ResponseEntity.ok().body(reviewService.createReview(landmarkId, reviewCreateRequestDto));
     }
 
     // 리뷰 조회 (+ 댓글 조회 함께)
     @GetMapping
-    public List<ReviewViewResponseDto> getReviews(@PathVariable("landmarkId") Long landmarkId){
-        return reviewService.getReviews();
+    public ResponseEntity<List<ReviewGetResponseDto>> getReview(@PathVariable("landmarkId") Long landmarkId){
+        return ResponseEntity.ok().body(reviewService.getReview(landmarkId));
     }
 
     // 리뷰 수정
     @PatchMapping("/{reviewId}")
-    public ReviewEditResponseDto patchReview(@PathVariable("landmarkId") Long landmarkId, @PathVariable("reviewId") Long id, @RequestBody ReviewEditRequestDto reviewEditRequestDto){
-        return reviewService.patchReview(landmarkId, id, reviewEditRequestDto);
+    public ResponseEntity<ReviewUpdateResponseDto> updateReview(@PathVariable("landmarkId") Long landmarkId, @PathVariable("reviewId") Long id, @RequestBody ReviewUpdateRequestDto reviewUpdateRequestDto){
+        return ResponseEntity.ok().body(reviewService.updateReview(landmarkId, id, reviewUpdateRequestDto));
     }
 
     // 리뷰 삭제
     @DeleteMapping("/{reviewId}")
-    public String deleteReview(@PathVariable("landmarkId") Long landmarkId, @PathVariable("reviewId") Long id, @RequestBody ReviewDeleteRequestDto reviewDeleteRequestDto){
-        return reviewService.deleteReview(landmarkId, id, reviewDeleteRequestDto);
+    public ResponseEntity<String> deleteReview(@PathVariable("landmarkId") Long landmarkId, @PathVariable("reviewId") Long id, @RequestBody ReviewDeleteRequestDto reviewDeleteRequestDto){
+        return ResponseEntity.ok().body(reviewService.deleteReview(landmarkId, id, reviewDeleteRequestDto));
 
     }
 }
