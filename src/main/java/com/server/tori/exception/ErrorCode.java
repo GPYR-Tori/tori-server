ec33eb3e8212f89aba7afecc25b06bfd904173cd
수현 <su551732@gmail.com>
2023-12-19 05:53:52 +0900

    :recycle:  refactor: 예외 고려 수정

--- src/main/java/com/server/tori/exception/ErrorCode.java
+++ src/main/java/com/server/tori/exception/ErrorCode.java
@@ -11,17 +11,24 @@ import static org.springframework.http.HttpStatus.*;
 public enum ErrorCode {
 
     // 400 BAD_REQUEST: 잘못된 요청
-    INVALID_PARAMETER(BAD_REQUEST, "입력 값을 확인해주세요."),
+
 
     // 401 UNAUTHORIZED: 인증되지 않은 사용자
-    UNAUTHORIZED_MEMBER(UNAUTHORIZED, "존재하지 않는 회원입니다."),
+
+
+    // 403 FORBIDDEN: 접근 권한이 없는 사용자
+    FORBIDDEN_REVIEW_USER(FORBIDDEN, "리뷰 권한이 없는 사용자입니다."),
+    FORBIDDEN_COMMENT_USER(FORBIDDEN, "댓글 권한이 없는 사용자입니다."),
+    NOT_IN_LANDMARK_REVIEW(FORBIDDEN, "해당 여행지에 속한 후기글이 아닙니다."),
 
     // 404 NOT_FOUND: 잘못된 리소스 접근
     MEMBER_NOT_FOUND(NOT_FOUND, "해당 회원 정보를 찾을 수 없습니다."),
-    LANDMARK_NOT_EXIST(NOT_FOUND, "해당 여행지 정보를 찾을 수 없습니다."),
+    LANDMARK_NOT_FOUND(NOT_FOUND, "해당 여행지 정보를 찾을 수 없습니다."),
+    REVIEW_NOT_FOUND(NOT_FOUND, "해당 리뷰를 찾을 수 없습니다."),
+    COMMENT_NOT_FOUND(NOT_FOUND, "해당 댓글을 찾을 수 없습니다."),
 
     // 409 CONFLICT: 중복된 리소스
-    DUPLICATE_EMAIL(CONFLICT, "해당 이메일은 이미 존재합니다."),
+
 
     // 500 INTERNAL SERVER ERROR
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "내부 서버 에러입니다.");
